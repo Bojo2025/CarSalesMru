4cb47bf47a4119a3515c3b0489e8344dcbc05e51
Bojo2025 <boyjonauth2022@gmail.com>
2026-09-07 16:27:22 +0400

    Add src/lib/facebook.ts

---
+++ src/lib/facebook.ts
@@ -0,0 +1,10 @@
+export const FB_MARKET_CARS =
+  "https://www.facebook.com/marketplace/106248356079603/cars/";
+
+export const FB_LOGIN_NEXT = `https://www.facebook.com/login.php?next=${encodeURIComponent(FB_MARKET_CARS)}`;
+
+export const FB_CITY_LINKS: Array<{ label: string; href: string }> = [
+  { label: "Port Louis", href: "https://www.facebook.com/marketplace/106248356079603/cars/" },
+  { label: "Curepipe", href: "https://www.facebook.com/marketplace/108648475832509/cars/" },
+  { label: "Quatre Bornes", href: "https://www.facebook.com/marketplace/106069382765976/cars/" },
+];
