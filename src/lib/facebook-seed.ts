312fa82e78e36553218d66bd2d66c4a03d56cd21
Bojo2025 <boyjonauth2022@gmail.com>
2026-09-07 16:27:19 +0400

    Add src/lib/facebook-seed.ts

---
+++ src/lib/facebook-seed.ts
@@ -0,0 +1,143 @@
+/** Public Facebook Marketplace car ads in Mauritius (indexed listings, last ~90 days). */
+export const FACEBOOK_INDEXED_ADS: Array<{ url: string; text: string }> = [
+  {
+    url: "https://www.facebook.com/marketplace/item/907513142393067/",
+    text: "TOYOTA CHR 2022 Automatic Full Options 1200cc Turbocharged Rs 1350000 Well Maintained Tel 57701022. Listed 1 week ago in Port Louis, Mauritius",
+  },
+  {
+    url: "https://www.facebook.com/marketplace/item/27869689812642153/",
+    text: "Toyota Corolla GZ Sport Year 2022 Automatic As from Rs 1,400,000 Colour Silver Tel 57722062 / 52552079 Port Louis, Mauritius",
+  },
+  {
+    url: "https://www.facebook.com/marketplace/item/1214320977399461/",
+    text: "TOYOTA RAIZE Transmission Auto Engine 1000CC Turbo Petrol Year 2019 Mileage 45000km Price Rs 950000 Beau Bassin, Plaines Wilhems, Mauritius",
+  },
+  {
+    url: "https://www.facebook.com/marketplace/item/5566869796870997/",
+    text: "Toyota CHR-GR Sport Year 2021 Automatic 1.8L Hybrid Mileage 67456 kms Contact 58045360 Used in Moka, Mauritius",
+  },
+  {
+    url: "https://www.facebook.com/marketplace/item/1287362205849091/",
+    text: "2020 Toyota Corolla Engine 1600cc Mileage 97K Transmission 6 speed manual Single owner Price Rs 840,000 Port Louis, Mauritius",
+  },
+  {
+    url: "https://www.facebook.com/marketplace/item/1966032284029401/",
+    text: "2020 Toyota C-HR 1.2L Turbo Petrol Automatic Transmission 70000 km Price Rs 1,195,000 slightly negotiable Port Louis, Mauritius",
+  },
+  {
+    url: "https://www.facebook.com/marketplace/item/921531194337607/",
+    text: "Toyota Corolla GZ Sport Year 2023 Engine 1800cc hybrid Mileage 25000 km Condition 10/10 Price Rs 1,620,000 negotiable Port Louis, Mauritius",
+  },
+  {
+    url: "https://www.facebook.com/marketplace/item/1937232667235303/",
+    text: "Toyota Vitz for sale MUR 295,000 Listed over a week ago Port Louis, Mauritius",
+  },
+  {
+    url: "https://www.facebook.com/marketplace/item/1755517002285243/",
+    text: "Toyota Aqua Price Rs 725,000 Leasing facilities available Listed over a week ago in Beau Bassin, Plaines Wilhems, Mauritius",
+  },
+  {
+    url: "https://www.facebook.com/marketplace/item/1530959668375242/",
+    text: "Toyota Aqua Hybrid 133000 km Perfect Condition Rs 485,000 MUR485,000 Mauritius",
+  },
+  {
+    url: "https://www.facebook.com/marketplace/item/4597884740447341/",
+    text: "For Sale TOYOTA AQUA Year 2022 1500cc Hybrid Rs 830000 negotiable Call 55390931 Les Pailles, Mauritius",
+  },
+  {
+    url: "https://www.facebook.com/marketplace/item/37164400683203227/",
+    text: "2019 Nissan Micra Rs 400,000 Local hatchback Petrol Automatic 111000 kms Port Louis, Mauritius",
+  },
+  {
+    url: "https://www.facebook.com/marketplace/item/2301143496917339/",
+    text: "Nissan Qashqai 2015 Sporty Automatic Diesel Full options 1600 Cc Rs 485000 Call 57214545 Mauritius",
+  },
+  {
+    url: "https://www.facebook.com/marketplace/item/1364928291635420/",
+    text: "Honda Civic FB Vtec Year 2015 1598cc Auto Mileage 119000 km Local Full options Auto light Rs 510000 negotiable Tel 57227248 Port Louis, Mauritius",
+  },
+  {
+    url: "https://www.facebook.com/marketplace/item/1059182383376285/",
+    text: "Honda Civic FD for sale MUR 260,000 Listed last Monday in Port Louis, Mauritius",
+  },
+  {
+    url: "https://www.facebook.com/marketplace/item/2024778624803767/",
+    text: "Honda Civic FD Year 2006 MUR 225,000 Port Louis, Mauritius Listed over a week ago",
+  },
+  {
+    url: "https://www.facebook.com/marketplace/item/1854504738563568/",
+    text: "Honda Vezel RS for sale MUR 900,000 Automatic Call 58314602 Listed over a week ago in Port Louis, Mauritius",
+  },
+  {
+    url: "https://www.facebook.com/marketplace/item/1013381547972194/",
+    text: "Honda Vezel RS year 2016 for sale complete as it is call 59276029 Listed over a week ago in Port Louis, Mauritius",
+  },
+  {
+    url: "https://www.facebook.com/marketplace/item/4452204035023346/",
+    text: "HONDA CIVIC Transmission Auto Engine 1400cc petrol Hybrid Year 2008 Mileage 177000 Price Rs 345000 negotiable Beau Bassin, Plaines Wilhems, Mauritius",
+  },
+  {
+    url: "https://www.facebook.com/marketplace/item/1440957214457632/",
+    text: "HONDA FIT HYBRID FOR SALE Year 2018 Automatic transmission 1500cc HYBRID Mileage 81000 kms One owner Full options Moka, Mauritius",
+  },
+  {
+    url: "https://www.facebook.com/marketplace/item/2000983207216835/",
+    text: "Honda Fit Shuttle 2022 87000kms Rs 895000 negotiable TEL 57485371 Vacoas Phoenix, Mauritius",
+  },
+  {
+    url: "https://www.facebook.com/marketplace/item/3573567002798584/",
+    text: "Mazda 3 2017 for Sale Engine 1600cc Petrol Gearbox Automatic Mileage 150000 Km Price Rs 585000 Rose Hill, Mauritius",
+  },
+  {
+    url: "https://www.facebook.com/marketplace/item/1274259527427195/",
+    text: "FOR SALE MAZDA 3 2016 Engine 1598CC Made in Japan Transmission Automatic Price Rs 515000 Call 57927442 Mauritius",
+  },
+  {
+    url: "https://www.facebook.com/marketplace/item/4873978692823629/",
+    text: "Mazda CX5 sport 33000km Bose sound system paddle shift electric seats Call 58127077 Port Louis, Mauritius",
+  },
+  {
+    url: "https://www.facebook.com/marketplace/item/1933071031010698/",
+    text: "SUZUKI SWIFT AUTOMATIC 48000 KM still under warranty Call 59181120 Grand Bel Air, Mauritius",
+  },
+  {
+    url: "https://www.facebook.com/marketplace/item/1535884495225448/",
+    text: "Automatic Suzuki Fronx GLX Year 2025 Full Options Keyless Low mileage Service at Agency Port Louis, Mauritius",
+  },
+  {
+    url: "https://www.facebook.com/marketplace/item/28057919870486120/",
+    text: "For sale suzuki alto 800 Year 2014 Well maintain Manual First owner 160000 km negotiable Contact 57137460 Mauritius",
+  },
+  {
+    url: "https://www.facebook.com/marketplace/item/1364994205522683/",
+    text: "BMW 316i for sale Year 2010 Mileage 120000km Call 57936770 Port Louis, Mauritius",
+  },
+  {
+    url: "https://www.facebook.com/marketplace/item/881635218143551/",
+    text: "Mercedes-Benz C180 Year 2011 Mileage 189000 km Transmission manual Price Rs 340000 negotiable Call 57984034 Mauritius",
+  },
+  {
+    url: "https://www.facebook.com/marketplace/item/28473305622277724/",
+    text: "BMW 116i Manual car for sale Year 2005 Good condition Call 57640097 Port Louis, Mauritius",
+  },
+  {
+    url: "https://www.facebook.com/marketplace/item/1073079805460364/",
+    text: "BMW 330e 2021 Automatic plug-in hybrid 2000cc Turbo 51000km One Owner Rs 1795000 Moka, Mauritius",
+  },
+  {
+    url: "https://www.facebook.com/marketplace/item/1095374059607141/",
+    text: "Mercedes-Benz CLA 200 URBAN Colour White Year 2017 Mileage 130000 km One owner no accident Quatre Bornes, Mauritius",
+  },
+  {
+    url: "https://www.facebook.com/marketplace/item/1596431845158308/",
+    text: "For sale Volkswagen polo 2016 Excellent condition Manual 1400 cc Rs 240000 negotiable Call 55054911 Port Louis, Mauritius",
+  },
+  {
+    url: "https://www.facebook.com/marketplace/item/981751304926338/",
+    text: "Mitsubishi GLX Year 2004 Motor 4G15 Full 1 year assurance Call 59174256 Curepipe, Mauritius",
+  },
+  {
+    url: "https://www.facebook.com/marketplace/item/2513083235806188/",
+    text: "SUV Hyundai Creta for sale Used MUR 635000 in Moka, Mauritius D'Epinay Pamplemousses",
+  },
+];
